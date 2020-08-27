8822be8e3cf331b984fb8e25f84ce8e970a60b73
Sungyong Cho <sucho@student.42seoul.kr>
2020-08-27 14:35:25 +0900

    ftps done

---
+++ srcs/ftps/srcs/run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+mkdir -p /etc/ssl/private
+mkdir -p /etc/ssl/certs
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj "/C=KO/ST=Seoul/L=Gaepodong/O=42Seoul/CN=ft_services"	-keyout /etc/ssl/private/vsftpd.key -out /etc/ssl/certs/vsftpd.crt
+
+mkdir -p /var/ftp
+
+adduser -D -h /var/ftp sucho
+echo "sucho:sucho" | chpasswd
+
+rc-status && touch /run/openrc/softlevel && rc-service vsftpd start
+
+vsftpd /etc/vsftpd/vsftpd.conf
