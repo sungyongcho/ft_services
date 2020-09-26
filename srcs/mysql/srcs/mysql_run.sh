6b77d340ef7c1c936ef827fc9433630b22309908
Sungyong Cho <sucho@student.42seoul.kr>
2020-09-27 04:38:13 +0900

    ftps, mysql done

---
+++ srcs/mysql/srcs/mysql_run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+mv /tmp/mariadb /etc/init.d/
+mv /tmp/mariadb-server.conf /etc/my.cnf.d/
+
+rc-status && touch /run/openrc/softlevel && rc-service mariadb setup
+
+mkdir /run/mysqld
+
+mysqld --user=root --bootstrap < /tmp/mysql_init.sql
+mysqld --user=root --console
