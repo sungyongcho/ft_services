39837605c5776e4615cf92a7fcc5d78060c86e7e
Sungyong Cho <sucho@student.42seoul.kr>
2020-10-04 16:26:44 +0900

    almost done

---
+++ delete.sh
@@ -0,0 +1,18 @@
+## delete dashboard
+kubectl delete -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.4/aio/deploy/recommended.yaml
+## delete metallb
+kubectl delete -f https://raw.githubusercontent.com/google/metallb/v0.8.1/manifests/metallb.yaml
+kubectl delete -f ./srcs/metallb_config.yaml
+
+## delete all components
+kubectl delete --all deployment
+kubectl delete --all services
+kubectl delete --all secret
+kubectl delete --all configmap
+kubectl delete --all pv,pvc
+
+##delete all docker images && containers (in case)
+docker rmi $(docker images -q)
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+docker rmi $(docker images -q)
