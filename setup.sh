c1270c21c1ec03630f1d4d85d51c358eeb3ef261
Sungyong Cho <sucho@student.42seoul.kr>
2020-10-07 19:31:39 +0900

    this is it

--- setup.sh
+++ setup.sh
@@ -8,34 +8,28 @@ echo "
 /_/  \__/____/____/\___/_/    |___/_/\___/\___/____/
        /_____/
 
-		                       (with minikube)
+		                    (with no minikube)
 		                            - by sucho
                             (sucho@student.42seoul.kr)\n\n"
 
 # 이미지 생성
 echo "hello"
-echo "kubernetes docker-desktop ver. script is available at ./tmp/ , in case."
-echo "gogo"
-
-# check & update phpymdamin and wordpress
 sh ./srcs/update_packages.sh
 
-# minikube
-minikube start --driver=virtualbox
-
-# MetalLB
-kubectl get configmap kube-proxy -n kube-system -o yaml | \
-sed -e "s/strictARP: false/strictARP: true/" | \
-kubectl diff -f - -n kube-system
-kubectl get configmap kube-proxy -n kube-system -o yaml | \
-sed -e "s/strictARP: false/strictARP: true/" | \
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
-kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-kubectl apply -f ./srcs/metallb_config.yaml
-
-# minikube pt. 2 - set minikube as k8s cluster env.
-eval $(minikube docker-env)
+#kubernetes dashboard
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.4/aio/deploy/recommended.yaml
+kubectl proxy & # & <-role of ampersand!!!
+sleep 3
+echo "access to below address with following token (str8 copy and paste)"
+echo "=====================token==================="
+echo "\n"
+kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep kubernetes-dashboard-token | awk '{print $1}') | grep token: | awk '{print $2}'
+echo "\n"
+echo "============================================="
+echo "\n"
+echo "==============dashboard address=============="
+echo "http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/"
+echo "============================================="
 
 # build images
 docker build -t service-nginx ./srcs/nginx/
@@ -49,6 +43,16 @@ docker build -t service-telegraf ./srcs/telegraf/
 
 echo "image built succesfully."
 
+# MetalLB
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+sed -e "s/strictARP: false/strictARP: true/" | \
+kubectl diff -f - -n kube-system
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+sed -e "s/strictARP: false/strictARP: true/" | \
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+kubectl apply -f ./srcs/metallb_config.yaml
 
 # for cluster
 kubectl create configmap nginx-configmap --from-file=./srcs/nginx/srcs/default.conf --from-file=./srcs/nginx/srcs/proxy.conf
@@ -62,10 +66,34 @@ kubectl apply -f ./srcs/influxdb/influxdb_conf.yaml
 kubectl apply -f ./srcs/grafana/grafana.yaml
 kubectl apply -f ./srcs/telegraf/telegraf.yaml
 
-minikube dashboard --url
+echo "
+ ____    _    ____  _   _ ____   ___    _    ____  ____
+|  _ \  / \  / ___|| | | | __ ) / _ \  / \  |  _ \|  _ \
+| | | |/ _ \ \___ \| |_| |  _ \| | | |/ _ \ | |_) | | | |
+| |_| / ___ \ ___) |  _  | |_) | |_| / ___ \|  _ <| |_| |
+|____/_/   \_\____/|_| |_|____/ \___/_/   \_\_| \_\____/
+"
+echo
+"
+ ____  _____ __  __ ___ _   _ ____  _____ ____
+|  _ \| ____|  \/  |_ _| \ | |  _ \| ____|  _ \
+| |_) |  _| | |\/| || ||  \| | | | |  _| | |_) |
+|  _ <| |___| |  | || || |\  | |_| | |___|  _ <
+|_| \_\_____|_|  |_|___|_| \_|____/|_____|_| \_\
+"
+echo "=====================token==================="
+echo "access to below address with following token (str8 copy and paste)"
+echo "=====================token==================="
+echo "\n"
+kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep kubernetes-dashboard-token | awk '{print $1}') | grep token: | awk '{print $2}'
+echo "\n"
+echo "============================================="
+echo "\n"
+echo "==============dashboard address=============="
+echo "http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/"
+echo "============================================="
 
 echo "Have Fun! ;)"
-
 echo "
     ______                            _
    / __/ /_     ________  ______   __(_)_______  _____
@@ -74,6 +102,6 @@ echo "
 /_/  \__/____/____/\___/_/    |___/_/\___/\___/____/
        /_____/
 
-		                       (with minikube)
+		                    (with no minikube)
 		                            - by sucho
                             (sucho@student.42seoul.kr)\n\n"
